5a1275a743ea5304780189898249c20a67be9d7d
Sohel Mia <sohelcse1999@gmail.com>
2019-11-09 15:30:07 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+	//slick slider active
+	
+  $('.slider-area').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  centerMode: true,
+	  variableWidth: true
+
+  });
+
+
+
+
+});
